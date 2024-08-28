7239d997166126528f7747085637aca74208c82e
Antonio Saleme <79271990+thusspokedata@users.noreply.github.com>
2024-08-28 15:17:42 +0200

    feat: create a Task (#4)

--- client/src/components/TaskCard.tsx
+++ client/src/components/TaskCard.tsx
@@ -5,6 +5,8 @@ export function TaskCard(task: Task) {
     <div>
       <h2>{task.title}</h2>
       <p>{task.description}</p>
+
+      <hr />
     </div>
   );
 }
--- client/src/pages/TaskFormPage.tsx
+++ client/src/pages/TaskFormPage.tsx
@@ -1,6 +1,47 @@
+import { useForm } from 'react-hook-form';
+import { createTask } from '../api/tasks.api';
+import { Task } from '../components';
+import { useNavigate } from 'react-router-dom';
+
 export function TaskFormPage() {
-    return (
-      <div>TaskFormPage</div>
-    )
-  }
-  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Task>();
+  const navigate = useNavigate();
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      await createTask(data);
+      navigate('/tasks');
+      console.log('Task created successfully!');
+    } catch (error) {
+      console.error('Failed to create task:', error);
+    }
+  });
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          {...register('title', { required: true })}
+        />{' '}
+        <br />
+        {errors.title && <span>Title is required</span>}
+        <br />
+        <textarea
+          rows={3}
+          placeholder="Description"
+          {...register('description', { required: true })}
+        />{' '}
+        <br />
+        {errors.description && <span>Description is required</span>}
+        <br />
+        <button type="submit">Create Task</button>
+      </form>
+    </div>
+  );
+}
